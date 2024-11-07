0cac12ee50a94e83cce3fb33302b3e2a5ad69764
Ibiyemi Abiodun <ibiyemi@intulon.com>
2024-11-07 13:05:04 -0500

    do not reject TLS certificates for IP addresses

--- packages/lib/serverConfig.ts
+++ packages/lib/serverConfig.ts
@@ -5,6 +5,12 @@ import { isENVDev } from "@calcom/lib/env";
 
 import { getAdditionalEmailHeaders } from "./getAdditionalEmailHeaders";
 
+function isIpAddress(ip: string): boolean {
+  const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
+  const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
+  return ipv4Pattern.test(ip) || ipv6Pattern.test(ip);
+}
+
 function detectTransport(): SendmailTransport.Options | SMTPConnection.Options | string {
   if (process.env.RESEND_API_KEY) {
     const transport = {
@@ -33,14 +39,15 @@ function detectTransport(): SendmailTransport.Options | SMTPConnection.Options |
             pass: process.env.EMAIL_SERVER_PASSWORD,
           }
         : undefined;
+    const host = process.env.EMAIL_SERVER_HOST;
 
     const transport = {
-      host: process.env.EMAIL_SERVER_HOST,
+      host,
       port,
       auth,
       secure: port === 465,
       tls: {
-        rejectUnauthorized: !isENVDev,
+        rejectUnauthorized: !isENVDev && !isIpAddress(host),
       },
     };
 
